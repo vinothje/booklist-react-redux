b0189153f08dad21fbdd12bffa862e55c59df949
Vinoth Jeevanandam <vinoth.j@gmail.com>
2017-07-01 20:31:50 -0700

    redux starter

---
+++ src/containers/book-list.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+    renderList() {
+        return this.props.books.map((book) => {
+       return (
+         <li className="list-group-item"
+             onClick={() => this.props.selectBook(book)}
+             key={book.title}>
+             {book.title}
+         </li>
+       );
+       });
+    }
+
+    render() {
+        return (
+          <ul className="list-group">
+              {this.renderList()}
+          </ul>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({selectBook : selectBook()}, dispatch);
+}
+
+function mapStateToProps(state) {
+    return {
+      books : state.books
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
+
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title : 'The Warriors'},
+        {title : 'Glory and Wisdom'},
+        {title : 'The Art of Science'},
+        {title : 'MicroApps'},
+        {title : 'Machine Language'},
+    ];
+}
